20163375190963ab1ce66bea786900f9824dda66
taohuayue <93538211+taohuayue@users.noreply.github.com>
2021-11-02 19:31:34 +0800

    更新

---
+++ PBaseDefense_Unity3D-master/Assets/P-BaseDefenseAssets/GameCode/SceneState/ISceneState.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 場景狀態
+public class ISceneState
+{
+	// 狀態名稱
+	private string m_StateName = "ISceneState";
+	public string StateName
+	{
+		get{ return m_StateName; }
+		set{ m_StateName = value; }
+	}
+
+	// 控制者
+	protected SceneStateController m_Controller = null;
+		
+	// 建構者
+	public ISceneState(SceneStateController Controller)
+	{ 
+		m_Controller = Controller; 
+	}
+
+	// 開始
+	public virtual void StateBegin()
+	{}
+
+	// 結束
+	public virtual void StateEnd()
+	{}
+
+	// 更新
+	public virtual void StateUpdate()
+	{}
+
+
+
+}
